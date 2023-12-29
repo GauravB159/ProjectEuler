c55f49c866760943c0a5333b5dcfbfdad04920d3
Gaurav Bhagchandani <gpunjabi28@gmail.com>
2023-12-29 05:13:46 -0500

    Problem #14

---
+++ 14/14.py
@@ -0,0 +1,28 @@
+from pelib import timed
+
+@timed
+def process(n):
+    cache = {}
+    def collatz(number):
+        if number == 1:
+            return 1
+        if cache.get(number, False):
+            return cache[number]
+        chain = 1
+        if number % 2 == 0:
+            chain += collatz(number / 2)
+        else:
+            chain += collatz(3 * number + 1)
+        cache[number] = chain
+        return chain
+    
+    max_ = 0
+    result = 0
+    for i in range(1, n):
+        chain = collatz(i)
+        if chain > max_:
+            max_ = chain
+            result = i
+    return result
+
+process(1000000)
