ec692afc9aff9c2810c5c2e32fbf7e7d87aab991
Gaurav Bhagchandani <gpunjabi28@gmail.com>
2023-12-29 07:03:30 -0500

    Problem #16

---
+++ 16/16.py
@@ -0,0 +1,20 @@
+from pelib import timed
+
+@timed
+def process(n, power):
+    answer = "1"
+    for _ in range(power):
+        next_answer = ""
+        carry = 0
+        for j, num in enumerate(answer):
+            pos = len(answer) - j - 1
+            next = int(answer[pos]) * n + carry
+            next_answer = str(next % 10) + next_answer
+            carry = next // 10
+        if carry > 0:
+            answer = str(carry) + next_answer
+        else:
+            answer = next_answer
+    return sum([int(x) for x in answer])
+
+process(2, 1000)
