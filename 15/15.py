ddb5b6659432ad79ecfc4596033c6295acf5874b
Gaurav Bhagchandani <gpunjabi28@gmail.com>
2023-12-29 05:51:36 -0500

    Problem #15

---
+++ 15/15.py
@@ -0,0 +1,20 @@
+from pelib import timed
+
+@timed
+def process(n):
+    cache = {}
+    def traverse(point):
+        if point.real > n or point.imag > n:
+            return 0
+        if point.real == n and point.imag == n:
+            return 1
+        if cache.get(point, 0):
+            return cache[point]
+        sum_ = 0
+        sum_ += traverse(point + 1j)
+        sum_ += traverse(point + 1)
+        cache[point] = sum_
+        return sum_
+    return traverse(0 + 0j)
+
+process(20)
