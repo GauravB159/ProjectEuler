87fcf4fe1e9a7a652f879080ffc9650c057fae67
Gaurav Bhagchandani <gpunjabi28@gmail.com>
2024-01-01 11:53:11 -0500

    Problem #25

--- 025/25.py
+++ 025/25.py
@@ -6,11 +6,10 @@ def process(n):
     fib1 = BigNum(1)
     fib2 = BigNum(1)
     count = 2
-    while(len(fib2.num) < 1000):
+    while(len(fib2.num) < n):
         fib2, fib1 = fib1.add(fib2), fib2
         count += 1
-    
-    print(count)
+    return count
 
 
 process(1000)
